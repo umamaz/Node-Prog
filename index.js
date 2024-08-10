adba2d42f59bc08edaba343506998596dd4b61e2
Umama <163664688+umamaz@users.noreply.github.com>
2024-08-10 23:52:19 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+
+
 const http = require("http");
-const myServer = http.createServer((req,res)=>{
-  console.log("New request Received");
-  res.end("Hello from Server");
+const fs = require("fs");
+const url = require("url");
+
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: ${req.url} Request received\n`;
+    const myUrl = url.parse(req.url);
+    console.log(myUrl);
+    fs.appendFile("./text.txt", log,(err) => {
+        if(err) throw err;
+        switch(req.url){
+            case '/':
+                res.end("Hello from HomePage");
+                break;
+            case '/about':
+                res.end("Hello from about");
+                break;  
+            default:
+                res.end("404 error found");  
+        }
+            
+    });
 });
 
-//Assigning Port to the server
-myServer.listen(8000, ()=> console.log("Server Started"));
+myServer.listen(8000, () => {
+    console.log("Server Started");
+});
